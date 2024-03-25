45e5e9f44141b506922c4b9e364f5b184a46fed6
Nguyen Ngoc Duc <nguyenngocduccl@gmail.com>
2024-03-25 10:31:30 +0700

    test merge

---
+++ HttpExample/app/src/main/java/com/example/httpexample/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.httpexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new NetworkTask().execute();
+    }
+
+
+
+}
+
